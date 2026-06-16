97661d33e8e5eb3fb180ce3c33f54cae5e0ba7cf
Rachana <shreerachanaa1010@gmail.com>
2026-06-16 15:27:00 +0530

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in range(len(nums)):
+            sum = 0
+            for j in range(i, len(nums)):
+                sum += nums[j]
+                if sum == k:
+                    res += 1
+        return res
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = curSum = 0
+        prefixSums = { 0 : 1 }
+
+        for num in nums:
+            curSum += num
+            diff = curSum - k
+
+            res += prefixSums.get(diff, 0)
+            prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+
+        return res 
